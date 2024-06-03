8c3dff8871ac9a5d3c6e0fb826a32b05f063c2d7
unknown <shauqiakmal8@gmail.com>
2024-06-03 21:44:59 +0800

    Initial commit of portfolio website

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// JavaScript for any interactivity or form handling can be added here
+document.addEventListener('DOMContentLoaded', function() {
+    // Example: alert on contact button click
+    document.querySelector('.contact-btn').addEventListener('click', function() {
+        alert('Contact form coming soon!');
+    });
+});
